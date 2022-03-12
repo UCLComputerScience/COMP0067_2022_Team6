59f653c9a2444aa57ccad5967eaeced60676abca
Elynor Kamil <elynorlucie@hotmail.com>
2022-03-12 16:51:37 +0000

    Update nav.blade.php
    Links added to different pages

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -4,15 +4,15 @@
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
-              <li class="nav-item"><a class="nav-link" href="about.html">Membership</a></li>
+              <li class="nav-item"><a class="nav-link" href="about">About</a></li>
+              <li class="nav-item"><a class="nav-link" href="membership">Membership</a></li>
                 <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle" id="navbarDropdownPortfolio" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Events</a>
                   <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownPortfolio">
-                      <li><a class="dropdown-item" href="portfolio-overview.html">Upcoming Events</a></li>
-                      <li><a class="dropdown-item" href="portfolio-item.html">Past Events</a></li>
+                      <li><a class="dropdown-item" href="events">Upcoming Events</a></li>
+                      <li><a class="dropdown-item" href="past-events">Past Events</a></li>
                     </ul>     </li>
-              <li class="nav-item"><a class="nav-link" href="faq.html">Contact Us</a></li>
+              <li class="nav-item"><a class="nav-link" href="contact">Contact Us</a></li>
               <li class="nav-item"><a class="nav-link" href="login">Login</a></li>
                 <li class="nav-item"><a class="nav-link" href="register">Register</a></li>
               
