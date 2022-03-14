7fd3947a53273d704f98a302f7cd2ada9f076d10
Jiahui Shi <93983336+SJH135@users.noreply.github.com>
2022-03-14 21:52:48 +0000

    about&contact change, laravel default nav bar remove

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -73,26 +73,22 @@
                         </div>
                     </div>
                     <!-- Contact cards-->
-                    <div class="row gx-5 row-cols-2 row-cols-lg-4 py-5">
+                    <div class="row gx-5 row-cols-2 row-cols-lg-3 py-5" align="center">
                         <div class="col">
-                            <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-chat-dots"></i></div>
-                            <div class="h5 mb-2">Chat with us</div>
-                            <p class="text-muted mb-0">Chat live with one of our support specialists.</p>
+                            <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-geo-alt"></i></div>
+                            <div class="h5 mb-2">Address</div>
+                            <p class="text-muted mb-0">4 Gateway Mews <br>London, N11 2UT</p>
                         </div>
                         <div class="col">
-                            <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-people"></i></div>
-                            <div class="h5">Ask the community</div>
-                            <p class="text-muted mb-0">Explore our community forums and communicate with other users.</p>
+                            <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-envelope"></i></div>
+                            <div class="h5">Email</div>
+                            <p class="text-muted mb-0">info@ancssc.org</p>
                         </div>
-                        <div class="col">
-                            <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-question-circle"></i></div>
-                            <div class="h5">Support center</div>
-                            <p class="text-muted mb-0">Browse FAQ's and support articles to find solutions.</p>
-                        </div>
+                        
                         <div class="col">
                             <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-telephone"></i></div>
-                            <div class="h5">Call us</div>
-                            <p class="text-muted mb-0">Call us during normal business hours at (555) 892-9403.</p>
+                            <div class="h5">Phone</div>
+                            <p class="text-muted mb-0">Call us during normal business hours<br>at +44 020 8368 8231.</p>
                         </div>
                     </div>
                 </div>
