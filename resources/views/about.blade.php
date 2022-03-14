fd4a25fa2408c9439c77e7b85f2b251b9a0d807b
Jiahui Shi <93983336+SJH135@users.noreply.github.com>
2022-03-14 21:16:16 +0000

    improve about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,9 +12,11 @@
                     <div class="row justify-content-center">
                         <div class="col-lg-8 col-xxl-6">
                             <div class="text-center my-5">
-                                <h1 class="fw-bolder mb-3">Our mission is to make building websites easier for everyone.</h1>
-                                <p class="lead fw-normal text-muted mb-4">Start Bootstrap was built on the idea that quality, functional website templates and themes should be available to everyone. Use our open source, free products, or support us by purchasing one of our premium products or services.</p>
-                                <a class="btn btn-primary btn-lg" href="#scroll-target">Read our story</a>
+                                <h2 class="fw-bolder mb-3">OUR VISION</h2>
+                                <p class="lead fw-normal text-muted mb-4">To empower civil society to establish a just world based on the values of South-South Cooperation. </p>
+                                <h2 class="fw-bolder mb-3">OUR MISSION</h2>
+                                <p class="lead fw-normal text-muted mb-4">To target the Sustainable Development Goals through supporting capacity-building, advocacy and knowledge sharing within the Global South and within Triangular Cooperation </p>
+                                <a class="btn btn-primary btn-lg" href="https://ancssc.com/">Learn More</a>
                             </div>
                         </div>
                     </div>
