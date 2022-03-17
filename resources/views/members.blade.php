ef3f6e210772aeee1f8e404ce3b19959b9e7fda7
mjwsolo <mjwsolo@hotmail.com>
2022-03-17 19:23:51 +0000

    added table to members page

--- resources/views/members.blade.php
+++ resources/views/members.blade.php
@@ -7,6 +7,60 @@
 <meta charset="utf-8" />
 
 @section('content')
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Users Table</title>
+
+    <!-- Meta -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+
+    <!-- Datatables CSS CDN -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+
+    <!-- jQuery CDN -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Datatables JS CDN -->
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+
+</head>
+<body>
+<h2 style="text-align: center;">Users Table</h2>
+<table id='usersTable' width='100%'>
+    <thead>
+    <tr>
+        <td>#ID</td>
+        <td>#Name</td>
+        <td>#Email</td>
+    </tr>
+    </thead>
+</table>
+
+<!-- Script -->
+<script type="text/javascript">
+    $(document).ready(function(){
+
+        // DataTable
+        $('#usersTable').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{route('users.getUsers')}}",
+            columns: [
+                { data: 'id' },
+                { data: 'name' },
+                { data: 'email' },
+            ]
+        });
+
+    });
+</script>
+</body>
+</html>
+
+
 <title>Google Maps Example</title>
 <style type="text/css">
 body { font: normal 14px Verdana; }
