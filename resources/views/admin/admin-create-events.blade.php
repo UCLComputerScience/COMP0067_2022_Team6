7e3c25f235d2c930bdda5ef58cf8de282e4c8ca6
Elynor Kamil <elynorlucie@hotmail.com>
2022-03-22 17:16:57 +0000

    Adding events, add timezone, events table update
    Adding new table for timezones, updating events table to include URL fields and remove language, adding new pages for adding events and editing

--- resources/views/admin/admin-create-events.blade.php
+++ resources/views/admin/admin-create-events.blade.php
@@ -1,7 +1,5 @@
-<!-- TODO: 
-1. Get filtering to work for projects table
-2. After filtering is working, copy-paste projects table section and change columns + contents for reports -->
-@extends('layouts.mainlayout-admin')
+@extends('layouts.mainlayout-logged-in')
+
 @section('content')
 
 <!DOCTYPE html>
@@ -12,30 +10,83 @@
             <!-- Page Content-->
             <section class="py-5">
                 <div class="text-center mb-5">
-                        <h1 class="fw-bolder">Create Events</h1>
+                        <h1 class="fw-bolder">Create Event</h1>
                         <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
  
 
-                        
-                        <div class="row mb-3">
-                            <label for="number_of_employees" class="col-md-4 col-form-label text-md-end">{{ __('Number of Employees') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="number_of_employees" type="text" class="form-control @error('number_of_employees') is-invalid @enderror" name="number_of_employees" value="{{ old('number_of_employees') }}" required autocomplete="number_of_employees" autofocus>
-
-                                @error('number_of_employees')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <?php 
+                      $username = Session::get('key');
+                      $username
+                      ?>
+                      
+                  
+                    
+                      <div class="container">
+                      
+                      <!-- Create auction form -->
+                      <div style="max-width: 800px; margin: 10px auto">
+                        <h2 class="my-3">Create new event </h2>
+                        <div class="card">
+                          <div class="card-body">
+                            <form method="post" enctype="multipart/form-data" action="admin-events-create-result">
+                                @csrf <!-- {{ csrf_field() }} -->
+                                <div class="form-group row">
+                                <label for="event_title" class="col-sm-2 col-form-label text-right">Event title</label>
+                                <div class="col-sm-10">
+                                  <input type="text" class="form-control" name="event_title" id="event_title" required minlength="20" placeholder="e.g. Well Building - Moldova">
+                                  <small id="titleHelp" class="form-text text-muted" text-align=left><span class="text-danger">* Required.</span> The title of your event (minimum 20 characters).</small>
+                                </div>
+                              </div>
+                              <div class="form-group row">
+                                <label for="event_description" class="col-sm-2 col-form-label text-right">Description</label>
+                                <div class="col-sm-10">
+                                  <textarea class="form-control" name="event_description" id="event_description" rows="4"></textarea>
+                                  <small id="detailsHelp" class="form-text text-muted">Full details of your event to give insight to members.</small>
+                                </div>
+                              </div>
+                              <div class="form-group row">
+                                <label for="event_datetime" class="col-sm-2 col-form-label text-right">Event date</label>
+                                <div class="col-sm-10">
+                                  <input type="datetime-local" class="form-control" name="projectEndDate" id="projectEndDate" required>
+                                  <small id="endDateHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Date and time this event will take place.</small>
+                                </div>
+                              </div>
+                              <!-- TODO: Add event_timezone as a field to the events table in DB -->
+                              <div class="form-group row">
+                                <label for="event_timezone" class="col-sm-2 col-form-label text-right">Event timezone</label>
+                                <div class="col-sm-10">
+                                  <select class="form-control" name="event_timezone" id="event_timezone" required>
+                                    <option value="">Choose a timezone</option>
+                                      <!-- TODO: Add 'timezone' table and populate it with values -->
+                                      <?php 
+                                    $result = DB::table('timezones')->get();    ?>
+                                    @foreach ($result as $row)
+                                        <option value="{{$row->timezones_id}}">{{$row->timezones_id}}</option>
+                                    @endforeach 
+                                      </select>
+                                      <small id="categoryHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Select the timezone this event will take place in.</small>
+                                </div>
+                              </div>
+                              <div class="form-group row">
+                              <label for="filesToUpload" class="col-sm-2 col-form-label text-right">Image upload</label>
+                              <div class="col-sm-10">
+                                <input type="file" name="filesToUpload[]" id="filesToUpload">
+                                <small id="filesToUploadHelp" class="form-text text-muted"><span class="text-danger">* Required.</span>Please upload one image for your event.</small>
+                              </div>
+                              </div>
+                              <div class="form-group row">
+                                <label for="event_call_url" class="col-sm-2 col-form-label text-right">Webcall URL for event</label>
+                                <div class="col-sm-10">
+                                  <textarea class="form-control" name="event_call_url" id="event_call_url" rows="4"></textarea>
+                                  <small id="detailsHelp" class="form-text text-muted">URL for the webcall where your event will take place e.g. Zoom link.</small>
+                                </div>
+                              </div>
+                              <button type="submit" id="submit" name="submit" class="btn btn-primary form-control">Create event</button>
+                            </form>
+                          </div>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="number_of_volunteers" class="col-md-4 col-form-label text-md-end">{{ __('Number of Volunteers') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="number_of_volunteers" type="text" class="form-control @error('number_of_volunteers') is-invalid @enderror" name="number_of_volunteers" value="{{ old('number_of_volunteers') }}" required autocomplete="number_of_volunteers" autofocus>
-
-                                
+                      </div>
+                      
+                      </div>
+                       
 @endsection
---
+++ resources/views/admin/admin-events-create-result.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.mainlayout-logged-in')
+
+@section('content')
+
+
+<div class="container my-5">
+
+<?php
+
+//require_once(app_path().'../vendor/j7mbo/twitter-api-php/TwitterAPIExchange.php');
+
+//require_once('resources/views/user/projects-create.blade.php');
+
+@include('admin-create-events');
+    
+    if (isset($_POST['submit']))
+    {
+
+    // Extracting the variables from the POST 
+    
+    $event_title = $_POST['event_title'];
+    $event_description = $_POST['event_description'];
+    $event_datetime = $_POST['event_datetime'];
+    $event_timezone = $_POST['event_timezone']; 
+    $event_url = $_POST['event_call_url'];
+    $userid = Auth::id();
+
+    // Putting them into an array
+
+    $newEventArray = array(
+        'event_title' => $event_title,
+        'event_description' => $event_description,
+        'event_datetime' => $event_datetime,
+        'event_timezone' => $event_timezone, 
+        'event_url' => $event_call_url,
+        'id' => $userid,
+    );
+ 
+    // The flow below checks for general errors in the form, then does 
+    // a loop for each image. 
+    
+    if (empty($event_title)){
+        echo 'Please enter an event title.';
+    } 
+    elseif (empty($event_description)){
+        echo 'Please enter an event description.';
+    } 
+    elseif (empty($event_datetime)){
+        echo 'Please select a date and time for your event.';
+    }
+    elseif (empty($event_timezone) and $event_datetime != 0){
+        echo 'Please enter a timezone for your event.';
+    }
+    elseif (empty($event_url)){
+        echo 'Please enter an event URL for people to join the event with.';
+    }
+    else {
+
+       $event_id =  DB::table('events')->insert($newEventArray);        
+        // loop for each uploaded file begins
+
+        foreach ($_FILES['filesToUpload']['name'] as $key => $value) {
+
+            // Creating the image path name
+            $target_dir = resource_path('views/user/images/');
+            
+            // Getting the filetype for the extension
+            $target_file = $target_dir . basename($_FILES["filesToUpload"]["name"][$key]);
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+            
+            // Creating a new name for the file based on a randomly generated unique ID
+            $imageUUID = uniqid('',false);
+            $targetFileDestination = $target_dir . $imageUUID . "." . $imageFileType;
+    
+            // Checking file size and type
+            if ($_FILES["filesToUpload"]["size"][$key] > 5000000) {
+                echo "Sorry, your file is too large.";
+            }
+            elseif($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+                echo "Sorry, only JPG, JPEG and PNG files are allowed.";
+            }
+            // if tests are successful, then the file is uploaded
+            else {
+                move_uploaded_file($_FILES["filesToUpload"]["tmp_name"][$key], $targetFileDestination);
+                //create_new_image_reference($eventID, $imageUUID, $imageFileType);
+                DB::table('ImagePaths')->insert(array(
+                    'event_id'     =>   $event_id, 
+                    'imageUUID'   =>   $imageUUID,
+                    'extension'   =>   $imageFileType)); }
+        }
+
+        echo '<div class="text-center">Event successfully created! <a href="events">View your new event.</a></div>';
+    }
+
+    }
+
+?>
+ @endsection
