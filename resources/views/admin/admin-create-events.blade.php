cb990f1b4957ee75a104fbcf296e2b48518d50e0
Elynor Kamil <elynorlucie@hotmail.com>
2022-03-23 17:15:39 +0000

    Updating admin event create
    Includes changing character minimum for event name to 10 (instead of 20), updating the help text for that, aligning all help text to the left, and updating timezone view so it shows the times relative to GMT.

--- resources/views/admin/admin-create-events.blade.php
+++ resources/views/admin/admin-create-events.blade.php
@@ -33,22 +33,22 @@
                                 <div class="form-group row">
                                 <label for="event_title" class="col-sm-2 col-form-label text-right">Event title</label>
                                 <div class="col-sm-10">
-                                  <input type="text" class="form-control" name="event_title" id="event_title" required minlength="20" placeholder="e.g. Well Building - Moldova">
-                                  <small id="titleHelp" class="form-text text-muted align-left"><span class="text-danger">* Required.</span> The title of your event (minimum 20 characters).</small>
+                                  <input type="text" class="form-control" name="event_title" id="event_title" required minlength="10" placeholder="e.g. Well Building - Moldova">
+                                  <small id="titleHelp" class="form-text text-muted align-left" style="float:left"><span class="text-danger">* Required.</span> The title of your event (minimum 10 characters).</small>
                                 </div>
                               </div>
                               <div class="form-group row">
                                 <label for="event_description" class="col-sm-2 col-form-label text-right">Description</label>
                                 <div class="col-sm-10">
                                   <textarea class="form-control" name="event_description" id="event_description" rows="4"></textarea>
-                                  <small id="detailsHelp" class="form-text text-muted">Full details of your event to give insight to members.</small>
+                                  <small id="detailsHelp" class="form-text text-muted" style="float:left">Full details of your event to give insight to members.</small>
                                 </div>
                               </div>
                               <div class="form-group row">
                                 <label for="event_datetime" class="col-sm-2 col-form-label text-right">Event date</label>
                                 <div class="col-sm-10">
                                   <input type="datetime-local" class="form-control" name="projectEndDate" id="projectEndDate" required>
-                                  <small id="endDateHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Date and time this event will take place.</small>
+                                  <small id="endDateHelp" class="form-text text-muted" style="float:left"><span class="text-danger">* Required.</span> Date and time this event will take place.</small>
                                 </div>
                               </div>
                               <div class="form-group row">
@@ -59,24 +59,24 @@
                                       <?php 
                                     $result = DB::table('timezones')->get();    ?>
                                     @foreach ($result as $row)
-                                        <option value="{{$row->timezone_id}}">{{$row->timezone_id}}</option>
+                                        <option value="{{$row->timezone_relative_to_gmt}}">{{$row->timezone_relative_to_gmt}}</option>
                                     @endforeach 
                                       </select>
-                                      <small id="categoryHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Select the timezone this event will take place in.</small>
+                                      <small id="categoryHelp" class="form-text text-muted" style="float:left"><span class="text-danger">* Required.</span> Select the timezone this event will take place in.</small>
                                 </div>
                               </div>
                               <div class="form-group row">
                               <label for="filesToUpload" class="col-sm-2 col-form-label text-right">Image upload</label>
                               <div class="col-sm-10">
                                 <input type="file" name="filesToUpload[]" id="filesToUpload">
-                                <small id="filesToUploadHelp" class="form-text text-muted"><span class="text-danger">* Required.</span>Please upload one image for your event.</small>
+                                <small id="filesToUploadHelp" class="form-text text-muted" style="float:left"><span class="text-danger">* Required.</span>Please upload one image for your event.</small>
                               </div>
                               </div>
                               <div class="form-group row">
                                 <label for="event_call_url" class="col-sm-2 col-form-label text-right">Webcall URL for event</label>
                                 <div class="col-sm-10">
                                   <textarea class="form-control" name="event_call_url" id="event_call_url" rows="4"></textarea>
-                                  <small id="detailsHelp" class="form-text text-muted">URL for the webcall where your event will take place e.g. Zoom link.</small>
+                                  <small id="detailsHelp" class="form-text text-muted" style="float:left">URL for the webcall where your event will take place e.g. Zoom link.</small>
                                 </div>
                               </div>
                               <button type="submit" id="submit" name="submit" class="btn btn-primary form-control">Create event</button>
