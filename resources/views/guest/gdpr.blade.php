873ebbcb04e82aa34114d8bb6f72b98496d5a834
Elynor Kamil <elynorlucie@hotmail.com>
2022-04-01 20:45:52 +0100

    Adding GDPR
    Includes:
    - New guest page with GDPR text (currently filler text)
    - Updating web.php to add the route to the new page
    - Updating registration page to change GDPR to a tickbox, forcing tickbox to be mandatory, and adding a link that opens the GDPR policy page on a new tab

---
+++ resources/views/guest/gdpr.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.mainlayout')
+@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<body>
+
+    <header class="bg-white py-5">
+
+        <div class="container px-5">
+            <div class="row gx-5 align-items-center justify-content-center">
+                <div class="col-lg-8 col-xl-7 col-xxl-6">
+                    <div class="my-5 text-center text-xl-start">
+
+                        <h1 class="display-5 fw-bolder text-dark mb-2">GDPR statement</h1>
+                        <p class="lead fw-normal text-dark-50 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mattis maximus consectetur. Sed sed cursus dui, in ultrices tortor. Donec posuere viverra ante non mollis. Pellentesque et tincidunt nibh. Quisque felis massa, bibendum sed finibus id, feugiat sed odio. Praesent dapibus lobortis facilisis. Vivamus eu tincidunt sapien. Cras sollicitudin, arcu non dictum consequat, erat nulla interdum lectus, id finibus ipsum nibh quis turpis. Mauris euismod augue quis posuere pretium. Vivamus est eros, ultrices eget ante vitae, varius sodales urna. Quisque eu felis sit amet nulla consequat venenatis sit amet at velit.</p>
+                        <p class="lead fw-normal text-dark-50 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mattis maximus consectetur. Sed sed cursus dui, in ultrices tortor. Donec posuere viverra ante non mollis. Pellentesque et tincidunt nibh. Quisque felis massa, bibendum sed finibus id, feugiat sed odio. Praesent dapibus lobortis facilisis. Vivamus eu tincidunt sapien. Cras sollicitudin, arcu non dictum consequat, erat nulla interdum lectus, id finibus ipsum nibh quis turpis. Mauris euismod augue quis posuere pretium. Vivamus est eros, ultrices eget ante vitae, varius sodales urna. Quisque eu felis sit amet nulla consequat venenatis sit amet at velit.</p>
+                        <!-- <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
+                            <a class="btn btn-primary btn-lg px-4 me-sm-3" href="register">Close tab</a>
+                        </div> -->
+                    </div>
+                </div>
+                <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center"><img class="img-fluid rounded-3 my-5" src="./assets/ANCSSClogo.jpg" width="85%" alt="..." /></div>
+            </div>
+        </div>
+    </header>   
+    
+
+</body>
+</html>
+@endsection
