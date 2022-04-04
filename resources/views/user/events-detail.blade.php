d409a95a9464cad83531b520707a61388b241eed
Elynor Kamil <elynorlucie@hotmail.com>
2022-04-04 11:23:40 +0100

    Tidying format

--- resources/views/user/events-detail.blade.php
+++ resources/views/user/events-detail.blade.php
@@ -4,55 +4,55 @@
 
 <?php  
 
-$event_id = Request::segment(2);
-$this_event = DB::Table('events')->select('event_id','event_title','event_description','event_datetime','event_timezone', 'event_call_url', 'event_video_url')->where('event_id',$event_id)->get();
-$event_title = $this_event->pluck('event_title');
-$event_description = $this_event->pluck('event_description');
-$event_datetime = $this_event->pluck('event_datetime');
-$event_timezone = $this_event->pluck('event_timezone');
-$event_call_url = $this_event->pluck('event_call_url');
-$event_call_url_test = $this_event->pluck('event_call_url');
-$event_video_url= $this_event->pluck('event_video_url');
-$first_image_path = DB::Table('ImagePaths')->select('imageUUID','extension')->where('event_id', $event_id)->get();
+    $event_id = Request::segment(2);
+    $this_event = DB::Table('events')->select('event_id','event_title','event_description','event_datetime','event_timezone', 'event_call_url', 'event_video_url')->where('event_id',$event_id)->get();
+    $event_title = $this_event->pluck('event_title');
+    $event_description = $this_event->pluck('event_description');
+    $event_datetime = $this_event->pluck('event_datetime');
+    $event_timezone = $this_event->pluck('event_timezone');
+    $event_call_url = $this_event->pluck('event_call_url');
+    $event_call_url_test = $this_event->pluck('event_call_url');
+    $event_video_url= $this_event->pluck('event_video_url');
+    $first_image_path = DB::Table('ImagePaths')->select('imageUUID','extension')->where('event_id', $event_id)->get();
 
-// If time, implement this to show if an event has already ended
-// Need to include if statement for if $now > $event_datetime to control what shows
-// $now = new DateTime();
-// if ($now > $event_datetime) {
-//         $time_remaining = 'This event has ended';
-//     }
+    // If time, implement this to show if an event has already ended
+    // Need to include if statement for if $now > $event_datetime to control what shows
+    // $now = new DateTime();
+    // if ($now > $event_datetime) {
+    //         $time_remaining = 'This event has ended';
+    //     }
 
 
 
-function strip_text($url){
-    $url = str_replace(array('[',']','"'), '', $url);
-    $url = stripslashes($url);
+    function strip_text($url){
+        $url = str_replace(array('[',']','"'), '', $url);
+        $url = stripslashes($url);
 
-    return $url;
-}
-
-$event_video_url = strip_text($event_video_url);
-$event_video_url = substr($event_video_url, strpos($event_video_url, "=") + 1);
-$event_call_url = strip_text($event_call_url);
-$event_description = strip_text($event_description);
-$event_title = strip_text($event_title);
-$event_datetime = strip_text($event_datetime);
-$event_datetime = substr($event_datetime, 0, -3);
-$event_timezone = strip_text($event_timezone);
-
-if ($event_video_url != 'null'){
-    $event_video_url = '
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/' .$event_video_url .'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        ';
+        return $url;
     }
 
+    $event_video_url = strip_text($event_video_url);
+    $event_video_url = substr($event_video_url, strpos($event_video_url, "=") + 1);
+    $event_call_url = strip_text($event_call_url);
+    $event_description = strip_text($event_description);
+    $event_title = strip_text($event_title);
+    $event_datetime = strip_text($event_datetime);
+    $event_datetime = substr($event_datetime, 0, -3);
+    $event_timezone = strip_text($event_timezone);
 
-if ($event_call_url != 'null'){
-        if (str_starts_with($event_call_url, 'https://') == FALSE){
-            $event_call_url = 'https://' .$event_call_url;
+    if ($event_video_url != 'null'){
+        $event_video_url = '
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/' .$event_video_url .'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            ';
         }
-    $event_call_url = '<br /><div class="d-grid"><a class="btn btn-primary" href="' .$event_call_url .'" target="_blank">Join event</a></div>';
-}
+
+
+    if ($event_call_url != 'null'){
+            if (str_starts_with($event_call_url, 'https://') == FALSE){
+                $event_call_url = 'https://' .$event_call_url;
+            }
+        $event_call_url = '<br /><div class="d-grid"><a class="btn btn-primary" href="' .$event_call_url .'" target="_blank">Join event</a></div>';
+    }
 
 
 
@@ -83,7 +83,8 @@ if ($event_call_url != 'null'){
                     <div class="row gx-5">
                         <div class="col-12"><img class="img-fluid rounded-3 mb-5" src="https://dummyimage.com/1300x700/343a40/6c757d" alt="..." /></div>
                         <br>
-                        <?php if ($event_video_url != 'null') {
+                        <?php echo $first_image_path;
+                        if ($event_video_url != 'null') {
                                 echo $event_video_url;
                             }
                         ?> 
