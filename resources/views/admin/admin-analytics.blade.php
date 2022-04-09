e2a2c61ecc122c55e06179e87077ee875f3add5c
mjwsolo <mjwsolo@hotmail.com>
2022-04-09 12:26:12 +0100

    added google analytics to Admin analytics page

--- resources/views/admin/admin-analytics.blade.php
+++ resources/views/admin/admin-analytics.blade.php
@@ -18,27 +18,10 @@
                         <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
  
 
-                        
-                        <div class="row mb-3">
-                            <label for="number_of_employees" class="col-md-4 col-form-label text-md-end">{{ __('Number of Employees') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="number_of_employees" type="text" class="form-control @error('number_of_employees') is-invalid @enderror" name="number_of_employees" value="{{ old('number_of_employees') }}" required autocomplete="number_of_employees" autofocus>
-
-                                @error('number_of_employees')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="number_of_volunteers" class="col-md-4 col-form-label text-md-end">{{ __('Number of Volunteers') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="number_of_volunteers" type="text" class="form-control @error('number_of_volunteers') is-invalid @enderror" name="number_of_volunteers" value="{{ old('number_of_volunteers') }}" required autocomplete="number_of_volunteers" autofocus>
-</section>
+                         <!-- Google Analytics -->
+                        <iframe width="600" height="450" src="https://datastudio.google.com/embed/reporting/119cc50a-5f86-4cd5-a396-6c4c88105278/page/Av8pC" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        <!-- Google Analytics -->
+            </section>
 </main>
 </div>
 </body>
