49111cf1bf1087d7b086fb7573bb72027760cf5f
Elynor Kamil <elynorlucie@hotmail.com>
2022-04-09 11:21:39 +0100

    Select current timezone by default

--- resources/views/admin/admin-events-edit.blade.php
+++ resources/views/admin/admin-events-edit.blade.php
@@ -25,6 +25,7 @@
 
                         $this_event_timezone_unstripped = DB::Table('events')->select('event_timezone')->where('event_id',$event_id)->get();
                         $this_event_timezone = strip_text($this_event_timezone_unstripped);
+                        $this_event_timezone = str_replace(array('{event_timezone:', '}'), '', $this_event_timezone);
 
                         $event_datetime_stripped = strip_text($event_datetime);
                         $event_timezone_stripped = strip_text($event_timezone);
@@ -82,10 +83,9 @@
                                     $result = DB::table('timezones')->get();    ?>
                                     @foreach ($result as $row)
                                       <option value="{{$row->timezone_relative_to_gmt}}">{{$row->timezone_relative_to_gmt}}</option>
-                                        <!-- if ({{$row->timezone_relative_to_gmt}} == $event_timezone){
-                                        } -->
                                     @endforeach
-                                    <!-- <option selected="{{$row->timezone_relative_to_gmt}}" >{{$row->timezone_relative_to_gmt}}</option> -->
+
+                                    <option selected="<?php echo $this_event_timezone;?>"><?php echo $this_event_timezone;?></option>
 
                                       </select>
                                       
