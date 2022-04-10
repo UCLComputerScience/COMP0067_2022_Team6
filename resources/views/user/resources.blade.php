49b85dcc21f53e6b1d5669e91e961456ae8c99c9
mjwsolo <mjwsolo@hotmail.com>
2022-04-10 20:42:49 +0100

    removed excess table from user/resources

--- resources/views/user/resources.blade.php
+++ resources/views/user/resources.blade.php
@@ -40,74 +40,9 @@ $user_id = Auth::id();
                     <div class="text-center mb-5">
                         <h1 class="fw-bolder">Resources</h1>
                         <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                    <div class="text-center mb-5">
-                        <h1 class="fw-bolder">ANCSSC Resources Area</h1>
                         <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     </div>
-                    <table id="resources" class="table table-striped table-hover" style="width:100%">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Description</th>
-                                <th>Uploaded By</th>
-                                <th>Language</th>
-                                <th>SDGs</th>
-                                <th>Upload Date</th>
-                                <th>Download</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Save Water</td>
-                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</td>
-                                <td>Garrett Winters</td>
-                                <td>English</td>
-                                <td>6,14</td>
-                                <td>2021/11/25</td>
-                                <td><u>Download Link</u></td>
-                            </tr>
-                            <tr>
-                                <td class="text-nowrap">Well Building Guide</td>
-                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</td>
-                                <td>Michael Bruce</td>
-                                <td>French</td>
-                                <td>3, 7, 11</td>
-                                <td>2019/04/13</td>
-                                <td><u>Download Link</u></td>
-                            </tr>
-                            <tr>
-                                <td>Protect Environment</td>
-                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</td>
-                                <td>Jonas Alexander</td>
-                                <td>English</td>
-                                <td>15</td>
-                                <td>2020/03/14</td>
-                                <td><u>Download Link</u></td>
-                            </tr>
-                            <tr>
-                                <td>Eradicate Poverty</td>
-                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</td>
-                                <td>Cara Stevens</td>
-                                <td>Arabic</td>
-                                <td>1, 8</td>
-                                <td>2022/02/08</td>
-                                <td><u>Download Link</u></td>
-                            </tr>
-                        </tbody>
-                        <tbody>
-                        <tfoot>
-                            <tr>
-                                <th>Title</th>
-                                <th>Description</th>
-                                <th>Uploaded By</th>
-                                <th>Language</th>
-                                <th>SDGs</th>
-                                <th>Upload Date</th>
-                                <th>Download</th>
-                            </tr>
-                        </tfoot>
-                    </table>
+
 
                     <div class="card-header">Resources List</div>
 
