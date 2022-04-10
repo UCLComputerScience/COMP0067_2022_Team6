c5101dea10b3909f06f2f67694487ba855a3c36b
Elynor Kamil <elynorlucie@hotmail.com>
2022-04-10 22:39:28 +0100

    Tidying of submit button for user-profile

--- resources/views/user/user-profile.blade.php
+++ resources/views/user/user-profile.blade.php
@@ -362,7 +362,7 @@
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" name="submit" id="submit" class="btn btn-primary">
                                     {{ __('Update my profile') }}
                                 </button>
                             </div>
