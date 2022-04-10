05cab9f14c8d38b309767318c8d987a7c40a15d2
mjwsolo <mjwsolo@hotmail.com>
2022-04-10 15:37:56 +0100

    updated create projects, and create projects result, and database to allow Google Autocomplete and multiple SDGs to be added to a project

--- ANCSSC_Database/ancssc_database v10.sql
+++ ANCSSC_Database/ancssc_database v10.sql
@@ -303,20 +303,41 @@ CREATE TABLE `projects` (
                             `projectValue` varchar(100) DEFAULT NULL,
                             `fundingRequired` varchar(100) DEFAULT NULL,
                             `sdg` varchar(255) DEFAULT NULL,
-                            `image_name` varchar(100) DEFAULT NULL
+                            `image_name` varchar(100) DEFAULT NULL,
+                            `latitude` float(10,6) NOT NULL,
+                            `longitude` float(10,6) DEFAULT NULL,
+                            `country` varchar(100) NOT NULL,
+                            `address` varchar(100) NOT NULL,
+                            `sdg1`int(20) DEFAULT NULL,
+                            `sdg2`int(20) DEFAULT NULL,
+                            `sdg3`int(20) DEFAULT NULL,
+                            `sdg4`int(20) DEFAULT NULL,
+                            `sdg5`int(20) DEFAULT NULL,
+                            `sdg6`int(20) DEFAULT NULL,
+                            `sdg7`int(20) DEFAULT NULL,
+                            `sdg8`int(20) DEFAULT NULL,
+                            `sdg9`int(20) DEFAULT NULL,
+                            `sdg10`int(20) DEFAULT NULL,
+                            `sdg11`int(20) DEFAULT NULL,
+                            `sdg12`int(20) DEFAULT NULL,
+                            `sdg13`int(20) DEFAULT NULL,
+                            `sdg14`int(20) DEFAULT NULL,
+                            `sdg15`int(20) DEFAULT NULL,
+                            `sdg16`int(20) DEFAULT NULL,
+                            `sdg17`int(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `projects`
 --
 
-INSERT INTO `projects` (`project_id`, `id`, `projectTitle`, `projectOrganisation`, `projectLocation`, `projectCity`, `projectCountry`, `projectDetails`, `project_date_added`, `project_last_updated`, `projectEndDate`, `projectValue`, `fundingRequired`, `sdg`, `image_name`) VALUES
-                                                                                                                                                                                                                                                                                     (3, 1, 'serfwerwiuoiuoiu', 'iojoijiojoij', 'oijoijioj', 'oijoijoij', 'oijoijoij', 'oijoijoij', 0, '0000-00-00', '2022-04-10 14:04:00', '8787887', '77', '3', NULL),
-                                                                                                                                                                                                                                                                                     (4, 2, 'The best project', 'The better one ', 'ereoihjoje ', 'joijoijoj', 'oijoijij', 'oijoijoijoij', 0, '0000-00-00', '2022-04-30 18:16:00', '4453', '22', '2', NULL),
-                                                                                                                                                                                                                                                                                     (5, 1, 'The greatest project The greatest project', 'werwerwerwrw', 'werwrerwrwer', 'werwerrwer', 'werwrer', 'werwerwerwr', 0, '0000-00-00', '2022-04-10 13:31:00', '3243442', '3434', '9', NULL),
-                                                                                                                                                                                                                                                                                     (6, 3, 'Improving the Efficiency of Metro Transport in India', ' Metro Transport in India', ' Metro Transport in India', ' Metro Transport in India', ' Metro Transport in India', ' Metro Transport in India', 0, '0000-00-00', '2022-04-30 14:50:00', '111', '111', '1', NULL);
+-- INSERT INTO `projects` (`project_id`, `id`, `projectTitle`, `projectOrganisation`, `projectLocation`, `projectCity`, `projectCountry`, `projectDetails`, `project_date_added`, `project_last_updated`, `projectEndDate`, `projectValue`, `fundingRequired`, `sdg`, `image_name`) VALUES
+--                                                                                                                                                                                                                                                                                      (3, 1, 'serfwerwiuoiuoiu', 'iojoijiojoij', 'oijoijioj', 'oijoijoij', 'oijoijoij', 'oijoijoij', 0, '0000-00-00', '2022-04-10 14:04:00', '8787887', '77', '3', NULL),
+--                                                                                                                                                                                                                                                                                      (4, 2, 'The best project', 'The better one ', 'ereoihjoje ', 'joijoijoj', 'oijoijij', 'oijoijoijoij', 0, '0000-00-00', '2022-04-30 18:16:00', '4453', '22', '2', NULL),
+--                                                                                                                                                                                                                                                                                      (5, 1, 'The greatest project The greatest project', 'werwerwerwrw', 'werwrerwrwer', 'werwerrwer', 'werwrer', 'werwerwerwr', 0, '0000-00-00', '2022-04-10 13:31:00', '3243442', '3434', '9', NULL),
+--                                                                                                                                                                                                                                                                                      (6, 3, 'Improving the Efficiency of Metro Transport in India', ' Metro Transport in India', ' Metro Transport in India', ' Metro Transport in India', ' Metro Transport in India', ' Metro Transport in India', 0, '0000-00-00', '2022-04-30 14:50:00', '111', '111', '1', NULL);
 
--- --------------------------------------------------------
+-- -- --------------------------------------------------------
 
 --
 -- Table structure for table `project_picture`
--- resources/views/user/projects-create.blade.php
+++ resources/views/user/projects-create.blade.php
@@ -4,7 +4,9 @@
 @extends('layouts.mainlayout-logged-in')
 
 @section('content')
-
+<head>
+  <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+</head>
 <!DOCTYPE html>
 <html lang="en">
     <body class="d-flex flex-column h-100">
@@ -53,6 +55,36 @@
                                   <small id="titleHelp" class="form-text text-muted"><span class="text-danger">* Required.</span>(maximum 5 characters)</small>
                                 </div>
                               </div>
+                            </div>
+                            <div class="form-group row mb-3">
+                                <label for="address" class="col-md-4 col-form-label text-md-end">{{ __('Address') }}</label>
+                                
+                                <div class="col-md-6">
+                                    <input id="address" type="text" name="address"  class="form-control @error('name') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address" autofocus placeholder="Enter Address">
+                                </div>
+                                @error('address')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            </div>
+                            <div class="col-md-6">
+                              <input id="latitude" type="hidden"  value="" name="latitude" class="form-control">
+                          </div>
+                      
+                          {{-- <div class="form-group row mb-3" id="longtitudeArea"> --}}
+                              {{-- <label for="longtitudeArea" class="col-md-4 col-form-label text-md-end"></label> --}}
+                              <div class="col-md-6">
+                              <input id="longitude" name="longitude" type="hidden" value=""   class="form-control">
+                              <div class="col-md-6">
+                              <input id="country" name="country" type="hidden" value="" class="form-control">
+                          </div>
+                          @error('address')
+                              <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                              </span>
+                          @enderror
+                      </div>
                               <div class="form-group row">
                                 <label for="auctionTitle" class="col-sm-2 col-form-label text-right">City</label>
                                 <div class="col-sm-10">
@@ -88,6 +120,61 @@
                                       <small id="categoryHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Select a category for this item.</small>
                                 </div>
                               </div>
+                              <div class="row mb-6">
+                                <div class="col-md-14">
+                                    <label class="form-check-label" for="sdg1">{{ __('1') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg1" name="sdg1">
+                                    <input class="form-check-input" type="checkbox" value="1" id="sdg1" name="sdg1">
+                                    <label class="form-check-label" for="sdg2">{{ __('2') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg2" name="sdg2">
+                                    <input class="form-check-input" type="checkbox" value="2" id="sdg2" name="sdg2">
+                                    <label class="form-check-label" for="sdg3">{{ __('3') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg3" name="sdg3">
+                                    <input class="form-check-input" type="checkbox" value="3" id="sdg3" name="sdg3">
+                                    <label class="form-check-label" for="sdg4">{{ __('4') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg4" name="sdg4">
+                                    <input class="form-check-input" type="checkbox" value="4" id="sdg4" name="sdg4">
+                                    <label class="form-check-label" for="sdg5">{{ __('5') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg5" name="sdg5">
+                                    <input class="form-check-input" type="checkbox" value="5" id="sdg5" name="sdg5">
+                                    <label class="form-check-label" for="sdg6">{{ __('6') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg6" name="sdg6">
+                                    <input class="form-check-input" type="checkbox" value="6" id="sdg6" name="sdg6">
+                                    <label class="form-check-label" for="sdg7">{{ __('7') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg7" name="sdg7">
+                                    <input class="form-check-input" type="checkbox" value="7" id="sdg7" name="sdg7">
+                                    <label class="form-check-label" for="sdg8">{{ __('8') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg8" name="sdg8">
+                                    <input class="form-check-input" type="checkbox" value="8" id="sdg8" name="sdg8">
+                                    <label class="form-check-label" for="sdg9">{{ __('9') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg9" name="sdg9">
+                                    <input class="form-check-input" type="checkbox" value="9" id="sdg9" name="sdg9">
+                                    <label class="form-check-label" for="sdg10">{{ __('10') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg10" name="sdg10">
+                                    <input class="form-check-input" type="checkbox" value="10" id="sdg10" name="sdg10">
+                                    <label class="form-check-label" for="sdg11">{{ __('11') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg11" name="sdg11">
+                                    <input class="form-check-input" type="checkbox" value="11" id="sdg11" name="sdg11">
+                                    <label class="form-check-label" for="sdg12">{{ __('12') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg12" name="sdg12">
+                                    <input class="form-check-input" type="checkbox" value="12" id="sdg12" name="sdg12">
+                                    <label class="form-check-label" for="sdg13">{{ __('13') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg13" name="sdg13">
+                                    <input class="form-check-input" type="checkbox" value="13" id="sdg13" name="sdg13">
+                                    <label class="form-check-label" for="sdg14">{{ __('14') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg14" name="sdg14">
+                                    <input class="form-check-input" type="checkbox" value="14" id="sdg14" name="sdg14">
+                                    <label class="form-check-label" for="sdg15">{{ __('15') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg15" name="sdg15">
+                                    <input class="form-check-input" type="checkbox" value="15" id="sdg15" name="sdg15">
+                                    <label class="form-check-label" for="sdg16">{{ __('16') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg16" name="sdg16">
+                                    <input class="form-check-input" type="checkbox" value="16" id="sdg16" name="sdg16">
+                                    <label class="form-check-label" for="sdg17">{{ __('17') }}</label>
+                                    <input class="form-check-input" type="hidden" value="" id="sdg17" name="sdg17">
+                                    <input class="form-check-input" type="checkbox" value="17" id="sdg17" name="sdg17">
+                                </div>
+                            </div>
                               <div class="form-group row">
                                 <label for="projectValue" class="col-sm-2 col-form-label text-right">Project Value</label>
                                 <div class="col-sm-10">
@@ -132,7 +219,48 @@
                           </div>
                         </div>
                       </div>
-                      
+                                       {{-- Google  --}}
+                 <body>
+
+                        {{-- <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                   --}}
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+                    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+                  
+                    <script type="text/javascript"
+                        src="https://maps.google.com/maps/api/js?key={{ env('GOOGLE_MAP_KEY') }}&libraries=places" ></script>
+                    <script>
+                        $(document).ready(function () {
+                            $("#latitudeArea").addClass("d-none");
+                            $("#longtitudeArea").addClass("d-none");
+                            $("#country").addClass("d-none");
+                        });
+                    </script>
+                    <script>
+
+                        google.maps.event.addDomListener(window, 'load', initialize);
+                  
+                        function initialize() {
+                            var input = document.getElementById('address');
+                            var autocomplete = new google.maps.places.Autocomplete(input);
+                  
+                            autocomplete.addListener('place_changed', function () {
+                                var place = autocomplete.getPlace();
+                                $('#latitude').val(place.geometry['location'].lat());
+                                $('#longitude').val(place.geometry['location'].lng());
+                                $('#country').val(place.address_components.slice(-1)[0].long_name); 
+                                // $('input[name="country"]').val(place.country.long_name); 
+
+                                $("#latitudeArea").removeClass("d-none");
+                                $("#longtitudeArea").removeClass("d-none");
+                                $("#country").removeClass("d-none");
+                            });
+                        }
+                    </script>
+                </body>
+                </html>
+                {{-- Google autocomplete --}}
                       </div>
 </div>
 </section>
