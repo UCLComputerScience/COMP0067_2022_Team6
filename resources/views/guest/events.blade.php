a66ef44707322f297728660bc4a8a49dd98edb22
Elynor Kamil <elynorlucie@hotmail.com>
2022-04-11 18:38:59 +0100

    Tickboxes for SDGs replaced cat dropdown, query setting updated

--- resources/views/guest/events.blade.php
+++ resources/views/guest/events.blade.php
@@ -1,5 +1,3 @@
-<!-- TODO: Move events into a single grid - currently they are separate sets which is not good -->
-
 @extends('layouts.mainlayout')
 
 @section('content')
@@ -7,18 +5,19 @@
 
 <div class="container">
 
-<h2 class="my-3">Events</h2>
+<h2 class="my-3 text-center">Events</h2>
 
 <div class="container">
+  <p class="lead fw-normal text-muted mb-0 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
 
-<h3 class="my-3">Browse events</h3>
+<h3 class="my-3 text-center">Browse Events</h3>
   <div id="searchSpecs">
   <!-- When this form is submitted, this PHP page is what processes it.
       Search/sort specs are passed to this page through parameters in the URL
       (GET method of passing data to a page). -->
 
   <form method="get" action="events">
-    <div class="row">
+  <div class="row">
       <div class="col-md-5 pr-0">
         <div class="form-group">
           <label for="keyword" class="sr-only">Search keyword:</label>
@@ -29,10 +28,26 @@
           </div>
         </div>
       </div>
-      <div class="col-md-3 pr-0">
-        
+      <div class="col-md-4 pr-0">
+        <div class="form-group">
+          <label for="cat" class="sr-only">Select SDG:</label>
+          <select class="form-control" id="cat" name="cat">
+            <option value="">Choose an option</option>
+                <?php 
+                  $result = DB::table('categories')->get();    ?>
+                    @foreach ($result as $row)
+                        <option value="{{$row->categoryID}}">{{$row->categoryName}}</option>
+                    @endforeach 
+                      </select>
+                  @error('SDGs')
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                      </span>
+                  @enderror
+          </select>
+        </div>
       </div>
-      <div class="col-md-3 pr-0">
+      <div class="col-md-2 pr-0">
         <div class="form-inline">
           <label class="mx-2" for="order_by">Sort by:</label>
           <select class="form-control" id="order_by" name = "order_by">
@@ -42,66 +57,8 @@
           </select>
         </div>
       </div>
-      <div class="col-md-6 pr-0">
-      <div class="form-group row">
-        <label for="sdg" class="col-sm-2 col-form-label text-right" for="sdg1">SDGs:</label>
-           <div class="col-md-10">
-              <label class="form-check-label" for="sdg1">{{ __('1') }}</label>
-              <!-- <input class="form-check-input" type="hidden" value="" id="sdg1" name="sdg1"> -->
-              <input class="form-check-input" type="checkbox" value="1" id="sdg1" name="sdg1">
-              <label class="form-check-label" for="sdg2">{{ __('2') }}</label>
-              <!-- <input class="form-check-input" type="hidden" value="" id="sdg2" name="sdg2"> -->
-              <input class="form-check-input" type="checkbox" value="2" id="sdg2" name="sdg2">
-              <label class="form-check-label" for="sdg3">{{ __('3') }}</label>
-              <input class="form-check-input" type="hidden" value="" id="sdg3" name="sdg3">
-              <input class="form-check-input" type="checkbox" value="3" id="sdg3" name="sdg3">
-              <label class="form-check-label" for="sdg4">{{ __('4') }}</label>
-              <input class="form-check-input" type="hidden" value="" id="sdg4" name="sdg4">
-              <input class="form-check-input" type="checkbox" value="4" id="sdg4" name="sdg4">
-              <label class="form-check-label" for="sdg5">{{ __('5') }}</label>
-              <input class="form-check-input" type="hidden" value="" id="sdg5" name="sdg5">
-              <input class="form-check-input" type="checkbox" value="5" id="sdg5" name="sdg5">
-              <label class="form-check-label" for="sdg6">{{ __('6') }}</label>
-              <input class="form-check-input" type="hidden" value="" id="sdg6" name="sdg6">
-              <input class="form-check-input" type="checkbox" value="6" id="sdg6" name="sdg6">
-              <label class="form-check-label" for="sdg7">{{ __('7') }}</label>
-              <input class="form-check-input" type="hidden" value="" id="sdg7" name="sdg7">
-              <input class="form-check-input" type="checkbox" value="7" id="sdg7" name="sdg7">
-              <label class="form-check-label" for="sdg8">{{ __('8') }}</label>
-              <input class="form-check-input" type="hidden" value="" id="sdg8" name="sdg8">
-              <input class="form-check-input" type="checkbox" value="8" id="sdg8" name="sdg8">
-              <label class="form-check-label" for="sdg9">{{ __('9') }}</label>
-              <input class="form-check-input" type="hidden" value="" id="sdg9" name="sdg9">
-              <input class="form-check-input" type="checkbox" value="9" id="sdg9" name="sdg9">
-              <label class="form-check-label" for="sdg10">{{ __('10') }}</label>
-              <input class="form-check-input" type="hidden" value="" id="sdg10" name="sdg10">
-              <input class="form-check-input" type="checkbox" value="10" id="sdg10" name="sdg10">
-              <label class="form-check-label" for="sdg11">{{ __('11') }}</label>
-              <input class="form-check-input" type="hidden" value="" id="sdg11" name="sdg11">
-              <input class="form-check-input" type="checkbox" value="11" id="sdg11" name="sdg11">
-              <label class="form-check-label" for="sdg12">{{ __('12') }}</label>
-              <input class="form-check-input" type="hidden" value="" id="sdg12" name="sdg12">
-              <input class="form-check-input" type="checkbox" value="12" id="sdg12" name="sdg12">
-              <label class="form-check-label" for="sdg13">{{ __('13') }}</label>
-              <input class="form-check-input" type="hidden" value="" id="sdg13" name="sdg13">
-              <input class="form-check-input" type="checkbox" value="13" id="sdg13" name="sdg13">
-              <label class="form-check-label" for="sdg14">{{ __('14') }}</label>
-              <input class="form-check-input" type="hidden" value="" id="sdg14" name="sdg14">
-              <input class="form-check-input" type="checkbox" value="14" id="sdg14" name="sdg14">
-              <label class="form-check-label" for="sdg15">{{ __('15') }}</label>
-              <input class="form-check-input" type="hidden" value="" id="sdg15" name="sdg15">
-              <input class="form-check-input" type="checkbox" value="15" id="sdg15" name="sdg15">
-              <label class="form-check-label" for="sdg16">{{ __('16') }}</label>
-              <input class="form-check-input" type="hidden" value="" id="sdg16" name="sdg16">
-              <input class="form-check-input" type="checkbox" value="16" id="sdg16" name="sdg16">
-              <label class="form-check-label" for="sdg17">{{ __('17') }}</label>
-              <input class="form-check-input" type="hidden" value="" id="sdg17" name="sdg17">
-              <input class="form-check-input" type="checkbox" value="17" id="sdg17" name="sdg17">
-          </div>
-</div>
-</div>
       <div class="col-md-1 px-0">
-        <button type="submit" class="btn btn-primary text-right" name="search"  value = "Search">Search</button>
+        <button type="submit" class="btn btn-primary" style="margin-top: 22%;" name="search"  value = "Search">Search</button>
       </div>
     </div>
   </form>
@@ -110,94 +67,36 @@
 <br \>
 
 <!-- Search results -->
-<?php
-  // Retrieve these from the URL
-
-  // if (!isset($_GET['page'])) {
-  //   $curr_page = 1;
-  // }
-  // else {
-  //   $curr_page = $_GET['page'];
-  // }
-
-
-
-
-
-  
-
-//   if ($ordering === "all"){
-//     $query .= " ORDER BY event_datetime DESC ";
-// }else if ($ordering === "upcoming"){
-//   $query .= " AND event_datetime >= GETDATE() 
-//   ORDER BY event_datetime DESC";
-// }else if($ordering === "past"){
-//   $query .= " AND event_datetime < GETDATE() 
-//   ORDER BY event_datetime DESC";
-// }
-
-  
-
-
-
-    // Calculate time to auction end
- // $now = new DateTime();
-  //if ($now > $end_time) {
-   // $time_remaining = 'This auction has ended';
-  //}
-  //else {
-    // Get interval:
-    //$time_to_end = date_diff($now, $end_time);
-   // $time_remaining = display_time_remaining($time_to_end) . ' remaining';
-  //}
-
-    //sql query to get the relevant information about the auction
-   /* $query = "SELECT tblauction.auctionID AS 'item_id',
-                         tblauction.itemName AS 'title',
-                         tblauction.itemDescription AS 'description',
-                         tblauction.endTime AS 'end_date',
-                         tblauction.startPrice AS 'start_price',
-                         tblbidding.bidPrice
-                         FROM tblAuction
-                         LEFT JOIN tblbidding
-                         ON tblauction.auctionID = tblbidding.auctionID
-                         WHERE ((tblAuction.itemDescription LIKE '%$keyword%')
-                         OR (tblAuction.itemName LIKE '%$keyword%')) ";*/
-    //if category is not set to be all then we want to filter our search further
-    //i.e. we want to add an AND statement after the above WHERE clause
-    /*if($category != "all"){
-      //the following query retrieves the itemCategoryID based on the category that was selected
-      $sql = "SELECT itemCategoryID FROM tblitemcategory WHERE itemCategory = '$category'";
-      $sql_result = mysqli_query($connection, $sql);
-      $sql_array = mysqli_fetch_assoc($sql_result);
-      $itemcategoryID = $sql_array['itemCategoryID'];
-      //filtering the search further
-      $query .= " AND tblauction.itemCategoryID = '$itemcategoryID'
-                  GROUP BY tblauction.auctionID ";
-    }else{
-      $query .= " GROUP BY tblauction.auctionID ";
-    }
-*/
-
-    //sql query for ordering the results based on datetime
-
-
-
-
-  
-  ?>
-
 <ul class="list-group">
 
 <?php
+// Some useful variables and functions for later if/else search conditions
 
-function strip_text($url){
-  $url = str_replace(array('"'), '', $url);
-  $url = stripslashes($url);
+$current_datetime = str(now());
 
-  return $url;
+function strip_text($var){
+  $var = str_replace(array('"'), '', $var);
+  $var = stripslashes($var);
+  return $var;
 }
 
+function strip_get($var){
+  $var = str_replace(array('->get();'), '', $var);
+  return $var;
+}
+
+
+  // Retrieve these from the URL
+
+  if (!isset($_GET['page'])) {
+    $curr_page = 1;
+  }
+  else {
+    $curr_page = $_GET['page'];
+  }
+
+
+// Processing search values inputted by user
 if(isset($_GET['search'])){
 
   if (!isset($_GET['keyword'])) {
@@ -213,40 +112,116 @@ else{
   $keyword = "";
 }
 
+// Checking which SDG has been chosen
+if (!isset($_GET['cat'])) {
+  $sdg = "";
+}
+else{
+  $sdg = $_GET['cat'];
+}
 
-
-// $my_projects = DB::Table('projects')->select('project_id','projectTitle','projectDetails','projectEndDate')->where('id',$userid)->get();
-
-
-// $events = DB::Table('events')->select('event_id','event_title','event_description','event_datetime', 'event_timezone')->get();
-
-
-
-// $query = DB::select("SELECT 'event_id', 'event_title', 'event_description', 'event_datetime', 'event_timezone', 'event_call_url', 'event_video_url',
-// 'sdg1', 'sdg2', 'sdg3', 'sdg4', 'sdg5', 'sdg6', 'sdg7', 'sdg8', 'sdg9', 'sdg10', 'sdg11', 'sdg12', 'sdg13', 'sdg14', 'sdg15', 'sdg16', 'sdg17' FROM `events`". $query)->get();
-
-echo "this is a ". $keyword;
-
-$query = DB::Table('events')->select('event_id', 'event_title', 'event_description', 'event_datetime', 'event_timezone', 'event_call_url', 'event_video_url',
+// Start of search query and conditions
+$query1 = DB::Table('events')->select('event_id', 'event_title', 'event_description', 'event_datetime', 'event_timezone', 'event_call_url', 'event_video_url',
 'sdg1', 'sdg2', 'sdg3', 'sdg4', 'sdg5', 'sdg6', 'sdg7', 'sdg8', 'sdg9', 'sdg10', 'sdg11', 'sdg12', 'sdg13', 'sdg14', 'sdg15', 'sdg16', 'sdg17')
-// ->where(function ($intermediate) {
-//   $intermediate->where('event_description', 'like', '%'.$keyword.'%')
-// ->orWhere('event_title', 'like', '%'.$keyword.'%')
-// ->orWhere('event_video_url', 'like', '%'.$keyword.'%')
-// ->orWhere('event_call_url', 'like', '%'.$keyword.'%');})
-// ->where('sdg1','=',1)
-->where('event_title', 'like', '%'.$keyword.'%', 'or', 'where', 'event_description', 'like', '%'.$keyword.'%')
-->get();
+->whereRaw("(event_title like '%$keyword%' or event_description like '%$keyword%')")
+->orderBy('event_datetime', 'desc');
+
+$query = $query1->get();
 
 
-if (isset($_GET['sdg1'])){
-  //  $query .= " AND 'sdg1' = 1";
-//  $query .= "->orWhere('sdg1','=',1)
-//  ->get();";}
-echo "this is working";}
-else{echo "nope";}
+if ($sdg == "1"){
+$query = $query1->where('sdg1',"1")->get();
+}
+
+elseif ($sdg == "2"){
+  $query = $query1->where('sdg2',"2")->get();
+}
+
+elseif ($sdg == "3"){
+  $query = $query1->where('sdg3',"3")->get();
+}
+
+elseif ($sdg == "4"){
+  $query = $query1->where('sdg4',"4")->get();
+}
+
+elseif ($sdg == "5"){
+  $query = $query1->where('sdg5',"5")->get();
+}
+
+elseif ($sdg == "6"){
+  $query = $query1->where('sdg6',"6")->get();
+}
+
+elseif ($sdg == "7"){
+  $query = $query1->where('sdg7',"7")->get();
+}
+
+elseif ($sdg == "8"){
+  $query = $query1->where('sdg8',"8")->get();
+}
+
+elseif ($sdg == "9"){
+  $query = $query1->where('sdg9',"9")->get();
+}
+
+elseif ($sdg == "10"){
+  $query = $query1->where('sdg10',"10")->get();
+}
+
+elseif ($sdg == "11"){
+  $query = $query1->where('sdg11',"11")->get();
+}
+
+elseif ($sdg == "12"){
+  $query = $query1->where('sdg12',"12")->get();
+}
+
+elseif ($sdg == "13"){
+  $query = $query1->where('sdg13',"13")->get();
+}
+
+elseif ($sdg == "14"){
+  $query = $query1->where('sdg14',"14")->get();
+}
+
+elseif ($sdg == "15"){
+  $query = $query1->where('sdg15',"15")->get();
+}
+
+elseif ($sdg == "16"){
+  $query = $query1->where('sdg16',"16")->get();
+}
+
+elseif ($sdg == "17"){
+  $query = $query1->where('sdg17',"17")->get();
+}
 
 
+if (!isset($_GET['order_by'])) {
+  $order_by = "all";
+}else {
+  $order_by = $_GET['order_by'];
+}
+
+  if ($order_by === "all"){
+    // $query .= " ORDER BY event_datetime DESC ";
+    $query->sortByDesc('event_datetime');
+    echo "<br>hello";
+}elseif ($order_by === "upcoming"){
+  // $query .= " AND event_datetime >= GETDATE() 
+  // ORDER BY event_datetime DESC";
+  $query = $query->where('event_datetime', '>=', $current_datetime);
+  // ->sortBy('event_datetime', 'desc');
+  // ->get();
+}elseif($order_by === "past"){
+  // $query .= " AND event_datetime < GETDATE() 
+  // -- ORDER BY event_datetime DESC";
+  $query = $query->where('event_datetime', '<', $current_datetime);
+  // ->orderBy('event_datetime', 'desc');
+  // ->get();
+}
+ 
 function print_event_with_image($event_id, $event_title, $event_description)
 {
 
@@ -291,9 +266,9 @@ $sdgs_first_strip = str_replace($array,"",$sdgs);
   }
   echo "</ul>";
   if ($counter > 0){
-    echo "Events: " . $counter;}
+    echo "<br>Events: " . $counter;}
   else{
-    echo "No events were found matching your search criteria";}
+    echo "<br>No events were found matching your search criteria";}
   
 ?>
 </div>
