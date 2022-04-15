824db606b2381c158c1945be09c65f1b57cbc6de
mjwsolo <mjwsolo@hotmail.com>
2022-04-15 23:50:44 +0100

    update env

--- ANCSSC_Database/ancssc_database v12.sql
+++ ANCSSC_Database/ancssc_database v12.sql
@@ -15,8 +15,8 @@ CREATE DATABASE ancssc_database
 
 GRANT ALL PRIVILEGES
     ON ancssc_database.*
-    TO ''@'localhost'
-        IDENTIFIED BY 'pass';
+    TO 'user'@'localhost'
+        IDENTIFIED BY 'password';
 
     -- TO 'user'@'localhost'
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
