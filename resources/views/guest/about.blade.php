7ce8274068af5a4c93bea2cc5c358c74e919f197
mjwsolo <mjwsolo@hotmail.com>
2022-04-19 10:16:51 +0100

    Update about.blade.php

--- resources/views/guest/about.blade.php
+++ resources/views/guest/about.blade.php
@@ -12,9 +12,9 @@
                     <div class="row justify-content-center">
                         <div class="col-lg-8 col-xxl-6">
                             <div class="text-center my-5">
-                                <h2 class="fw-bolder mb-3">Our Vision</h2>
+                                <h2 class="fw-bolder">Our Vision</h2>
                                 <p class="lead fw-normal text-muted mb-4">To empower civil society to establish a just world based on the values of South-South Cooperation. </p>
-                                <h2 class="fw-bolder mb-3">Our Mission</h2>
+                                <h2 class="fw-bolder">Our Mission</h2>
                                 <p class="lead fw-normal text-muted mb-4">To target the Sustainable Development Goals through supporting capacity-building, advocacy and knowledge sharing within the Global South and within Triangular Cooperation </p>
                                 <!-- <a class="btn btn-primary btn-lg" href="https://ancssc.com/">Learn More</a> -->
                             </div>
@@ -28,7 +28,7 @@
                     <div class="row gx-5 align-items-center">
                         <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0" src="./assets/ANCSSClogo.jpg" alt="..." /></div>
                         <div class="col-lg-6">
-                            <h2 class="fw-bolder">Our founding</h2>
+                            <h2 class="fw-bolder">Our Founding</h2>
                             <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae et fugit cupiditate, quibusdam expedita, maiores eaque quisquam.</p>
                         </div>
                     </div>
@@ -40,7 +40,7 @@
                     <div class="row gx-5 align-items-center">
                         <div class="col-lg-6 order-first order-lg-last"><img class="img-fluid rounded mb-5 mb-lg-0" src="https://ancssc.com/wp-content/uploads/event-manager-uploads/event_banner/2020/09/FORUM-IDA.png" alt="..." /></div>
                         <div class="col-lg-6">
-                            <h2 class="fw-bolder">Growth &amp; beyond</h2>
+                            <h2 class="fw-bolder">Growth &amp; Beyond</h2>
                             <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae et fugit cupiditate, quibusdam expedita, maiores eaque quisquam.</p>
                         </div>
                     </div>
