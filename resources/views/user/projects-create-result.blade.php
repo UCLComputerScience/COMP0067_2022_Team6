efde51a08e0188b7a8acfbb40b700905a4484090
Jiahui Shi <93983336+SJH135@users.noreply.github.com>
2022-04-22 12:12:09 +0100

    Update projects-create-result.blade.php

--- resources/views/user/projects-create-result.blade.php
+++ resources/views/user/projects-create-result.blade.php
@@ -110,38 +110,7 @@
 
 
     
-        // loop for each uploaded file begins
-
-        // foreach ($_FILES['filesToUpload']['name'] as $key => $value) {
-
-        //     // Creating the image path name
-        //     $target_dir = resource_path('views/user/images/');
-            
-        //     // Getting the filetype for the extension
-        //     $target_file = $target_dir . basename($_FILES["filesToUpload"]["name"][$key]);
-        //     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-            
-        //     // Creating a new name for the file based on a randomly generated unique ID
-        //     $imageUUID = uniqid('',false);
-        //     $targetFileDestination = $target_dir . $imageUUID . "." . $imageFileType;
-    
-        //     // Checking file size and type
-        //     if ($_FILES["filesToUpload"]["size"][$key] > 5000000) {
-        //         echo "Sorry, your file is too large.";
-        //     }
-        //     elseif($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-        //         echo "Sorry, only JPG, JPEG and PNG files are allowed.";
-        //     }
-        //     // if tests are successful, then the file is uploaded
-        //     else {
-        //         move_uploaded_file($_FILES["filesToUpload"]["tmp_name"][$key], $targetFileDestination);
-        //         //create_new_image_reference($projectID, $imageUUID, $imageFileType);
-        //         $imageUUID = hexdec($imageUUID);
-        //         DB::table('ImagePaths')->insert(array(
-        //             'project_id'     =>   $project_id, 
-        //             'imageUUID'   =>   $imageUUID,
-        //             'extension'   =>   $imageFileType)); }
-        // }
+        
 
         echo '<section class="bg-light-py-5">
         <br>
